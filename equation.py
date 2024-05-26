3ca6fad64018599cc45c193bf6ad48fbac84c932
dan18050182@gmail.com <dan18050182@gmail.com>
2024-05-26 18:49:33 +0400

    FIX

--- equation.py
+++ equation.py
@@ -3,8 +3,8 @@ def dx1t(x1, bpn, bpk, polx3, polx19, polx27, polx21, polx22,polx23,polx29):
     return res
 
 
-def dx2t(x2, mpn, x2x4, x2x20, x2x28, mpk, x2x22, x2x23, x2x29):
-    return (((mpn / x2) * x2x4 * x2x20 * x2x28) - ((mpk / x2) * x2x22 * x2x23 * x2x29))
+def dx2t(x2, mpn, x2x4, polx20, polx28, mpk, polx22, polx23, polx29):
+    return (mpn * x2x4 * polx20 * polx28) - (mpk * polx22 * polx23 * polx29)
 
 
 def dx3t(x3, bv, bz, x3x19, bc):
@@ -12,8 +12,8 @@ def dx3t(x3, bv, bz, x3x19, bc):
     return res
 
 
-def dx4t(x4, mv, mz, x4x20, mc):
-    return x4 * ((mv / mz * x4x20) - (mc / mz))
+def dx4t(mv, mz, polx20, mc):
+    return ((mv / mz * polx20) - (mc / mz))
 
 
 def dx5t(x5, c, p):
@@ -21,8 +21,8 @@ def dx5t(x5, c, p):
     return res
 
 
-def dx6t(x6, pt, x6x12, pi, x6x8, pj, x6x10, pd, x6x17, ps):
-    return x6 * ((pt * x6x12) + (pi * x6x8) + (pj * x6x10) + (pd * x6x17) + ps)
+def dx6t(pt, polx12, pi, polx8, pj, polx10, pd, polx17, ps, polx11, polx25,polx30):
+    return ((pt * polx12) + (pi * polx8) + (pj * polx10) + (pd * polx17) + ps) * polx11 * polx25 * polx30
 
 
 def dx7t(x7, g, ig, ng, f, x7x14):
@@ -30,8 +30,8 @@ def dx7t(x7, g, ig, ng, f, x7x14):
     return res
 
 
-def dx8t(x8, an, ak, p):
-    return x8 * ((an - ak) / p)
+def dx8t(an, ak, p):
+    return (an - ak) / p
 
 
 def dx9t(x9, vn, vk, p, x9x5, x9x6):
@@ -39,8 +39,8 @@ def dx9t(x9, vn, vk, p, x9x5, x9x6):
     return res
 
 
-def dx10t(x10, idpn, idpk, p):
-    return x10 * ((idpn - idpk) / p)
+def dx10t(idpn, idpk, p):
+    return (idpn - idpk) / p
 
 
 def dx11t(x11, dpn, dpk):
@@ -48,8 +48,8 @@ def dx11t(x11, dpn, dpk):
     return res
 
 
-def dx12t(x12, bpn, bpk, p):
-    return x12 * ((bpn - bpk) / p)
+def dx12t(bpn, bpk, p):
+    return (bpn - bpk) / p
 
 
 def dx13t(x13, IB, IN, IL, F):
@@ -57,8 +57,8 @@ def dx13t(x13, IB, IN, IL, F):
     return res
 
 
-def dx14t(x14, ds, x14x15, dv, x14x16, dp):
-    return x14 * ((ds * x14x15) + (dv * x14x16) + dp)
+def dx14t(ds, polx15, dv, polx16, dp):
+    return (ds * polx15) + (dv * polx16) + dp
 
 
 def dx15t(x15, jdn, jdk, p):
@@ -66,8 +66,8 @@ def dx15t(x15, jdn, jdk, p):
     return res
 
 
-def dx16t(x16, inn, ink, npp):
-    return x16 * ((inn - ink) / npp)
+def dx16t(inn, ink, npp):
+    return (inn - ink) / npp
 
 
 def dx17t(x17, ppn, ppk, p):
@@ -75,8 +75,8 @@ def dx17t(x17, ppn, ppk, p):
     return res
 
 
-def x18(x18, allIncomeProf, allIncome, x18x26):
-    return x18 * ((allIncomeProf / allIncome) * x18x26)
+def x18(allIncomeProf, allIncome, polx26):
+    return (allIncomeProf / allIncome) * polx26
 
 
 def dx19t(x19, pbn, pbk, pb):
@@ -84,8 +84,8 @@ def dx19t(x19, pbn, pbk, pb):
     return res
 
 
-def dx20t(x20, pmn, pmk, pm):
-    return x20 * ((pmn - pmk) / pm)
+def dx20t(pmn, pmk, pm):
+    return (pmn - pmk) / pm
 
 
 def dx21t(x21, ipn, ipk):
@@ -93,8 +93,8 @@ def dx21t(x21, ipn, ipk):
     return res
 
 
-def dx22t(x22, opn, opk):
-    return x22 * ((opn / x22) - (opk / x22))
+def dx22t(opn, opk):
+    return opn  - opk
 
 
 def dx23t(x23, jpn, jpk):
@@ -102,8 +102,8 @@ def dx23t(x23, jpn, jpk):
     return res
 
 
-def dx24t(x24, ispn, ispk, x24x26):
-    return x24 * ((ispn / x24) - ((ispk / x24) * x24x26))
+def dx24t(ispn, ispk, polx26):
+    return ispn - (ispk * polx26)
 
 
 def dx25t(x25, jpn, jpk, p):
@@ -111,8 +111,8 @@ def dx25t(x25, jpn, jpk, p):
     return res
 
 
-def dx26t(x26, idpn, x26x21, idpk):
-    return x26 * (((idpn / x26) * x26x21) - (idpk / x26))
+def dx26t(idpn, polx21, idpk):
+    return (idpn * polx21) - idpk
 
 
 def dx27t(x27, brpn, brpk):
@@ -120,8 +120,8 @@ def dx27t(x27, brpn, brpk):
     return res
 
 
-def dx28t(x28, mrpn, mrpk):
-    return x28 * ((mrpn / x28) - (mrpk / x28))
+def dx28t(mrpn, mrpk):
+    return mrpn  - mrpk
 
 
 def dx29t(x29, srpn, srpk):
@@ -129,8 +129,8 @@ def dx29t(x29, srpn, srpk):
     return res
 
 
-def dx30t(x30, rpn, rpk, p):
-    return x30 * ((rpn - rpk) / p)
+def dx30t(rpn, rpk, p):
+    return (rpn - rpk) / p
 
 
 def dx31t(x31, ir, nr, f, x31x30):
