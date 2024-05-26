4617e0dc568b22773580e28ca5aaae47538f27a5
dan18050182@gmail.com <dan18050182@gmail.com>
2024-05-26 19:26:28 +0400

    FIX

--- main.py
+++ main.py
@@ -60,30 +60,6 @@ class Form(QWidget):
         f16 = self.polinom(self.inputs["x5_input"], self.inputs["f16k1_input"], self.inputs["f16k2_input"],self.inputs["f16k3_input"], self.inputs["f16k4_input"])
         f18 = self.polinom(self.inputs["x6_input"], self.inputs["f18k1_input"], self.inputs["f18k2_input"],self.inputs["f18k3_input"], self.inputs["f18k4_input"])
         f34 = self.polinom(self.inputs["x30_input"], self.inputs["f34k1_input"], self.inputs["f34k2_input"],self.inputs["f34k3_input"], self.inputs["f34k4_input"])
-        f1 = self.polinom(self, self.inputs["x3_input"], self.inputs["f1k1_input"], self.inputs["f1k2_input"],
-                          self.inputs["f1k3_input"], self.inputs["f1k4_input"])
-        f2 = self.polinom(self, self.inputs["x19_input"], self.inputs["f2k1_input"], self.inputs["f2k2_input"],
-                          self.inputs["f2k3_input"], self.inputs["f2k4_input"])
-        f3 = self.polinom(self, self.inputs["x21_input"], self.inputs["f3k1_input"], self.inputs["f3k2_input"],
-                          self.inputs["f3k3_input"], self.inputs["f3k4_input"])
-        f4 = self.polinom(self, self.inputs["x22_input"], self.inputs["f4k1_input"], self.inputs["f4k2_input"],
-                          self.inputs["f4k3_input"], self.inputs["f4k4_input"])
-        f5 = self.polinom(self, self.inputs["x23_input"], self.inputs["f5k1_input"], self.inputs["f5k2_input"],
-                          self.inputs["f5k3_input"], self.inputs["f5k4_input"])
-        f6 = self.polinom(self, self.inputs["x27_input"], self.inputs["f6k1_input"], self.inputs["f6k2_input"],
-                          self.inputs["f6k3_input"], self.inputs["f6k4_input"])
-        f7 = self.polinom(self, self.inputs["x29_input"], self.inputs["f7k1_input"], self.inputs["f7k2_input"],
-                          self.inputs["f7k3_input"], self.inputs["f7k4_input"])
-        f14 = self.polinom(self, self.inputs["x19_input"], self.inputs["f14k1_input"], self.inputs["f14k2_input"],
-                           self.inputs["f14k3_input"], self.inputs["f14k4_input"])
-        f25 = self.polinom(self, self.inputs["x14_input"], self.inputs["f25k1_input"], self.inputs["f25k2_input"],
-                           self.inputs["f25k3_input"], self.inputs["f25k4_input"])
-        f16 = self.polinom(self, self.inputs["x5_input"], self.inputs["f16k1_input"], self.inputs["f16k2_input"],
-                           self.inputs["f16k3_input"], self.inputs["f16k4_input"])
-        f18 = self.polinom(self, self.inputs["x6_input"], self.inputs["f18k1_input"], self.inputs["f18k2_input"],
-                           self.inputs["f18k3_input"], self.inputs["f18k4_input"])
-        f34 = self.polinom(self, self.inputs["x30_input"], self.inputs["f34k1_input"], self.inputs["f34k2_input"],
-                           self.inputs["f34k3_input"], self.inputs["f34k4_input"])
 
         f8 = self.polinom(self.inputs["x4_input"], self.inputs["f8k1_input"], self.inputs["f8k2_input"],
                           self.inputs["f8k3_input"], self.inputs["f8k4_input"])
